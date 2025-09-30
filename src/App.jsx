bf15789f62ff3173cc847a3766ea49ce41577023
fabian <180667352+fabo0822@users.noreply.github.com.>
2025-09-30 13:01:54 -0500

    feat: :sparkles: Start Proyect

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'; // Importamos el CSS para estilos .
+
+function App() {
+  return (
+    <div className="App">
+      {/* Aquí pondremos nuestro navbar y las secciones. Por ahora, solo un texto de prueba. */}
+      <h1>¡Hola, este es mi app de contactos!</h1>
+    </div>
+  );
+}
+
+export default App; // Esto hace que el componente sea usable en otros archivos
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+// Este es un componente separado para el navbar. En React, dividimos la UI en piezas pequeñas para reutilizarlas.
+
+function Navbar() {
+   
+}
+
+export default Navbar;
